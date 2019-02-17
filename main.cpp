55ce42bf65a3dd73e8c4875abbb0e7ba32e5624c
alex <lu77al@gmail.com>
2019-02-17 11:19:06 +0200

    Blocks addresses checked

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ using std::cin;
 using std::endl;
 
 void showDump(const char* header, const char* p, int n);
+void showOffset(char* p[], char* base, int n);
 
 int main(void)
 {
@@ -19,12 +20,13 @@ int main(void)
 	Heap heap(mem, 200, 15);
 	showDump("dump: ", mem, 15);
 	cout << "\n3. Allocate 12 blocks\n";
-	char *obj[12];
+	char* obj[12];
 	for (int i = 0; i < 12; i++)
 	{
 		obj[i] = (char*)heap.allocate();
 		showDump("alloction: ", mem, 15);
 	}
+	showOffset(obj, mem, 12);
 	cout << "\n4. Free them all\n";
 	for (int i = 0; i < 12; i++)
 	{
@@ -42,4 +44,13 @@ void showDump(const char* header, const char* p, int n)
 		cout << +p[i] << ", ";
 	}
 	cout << endl;
+}
+
+void showOffset(char* p[], char* base, int n)
+{
+	cout << "Blocks offset: ";
+	for (int i = 0; i < n; i++)
+	{
+		cout << +(p[i] - base) << ((i < n - 1) ? ", " : "\n");
+	}
 }
