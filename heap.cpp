5ae8d8ed2642ebd1a3f5e3dada1ac14511769c85
alex <lu77al@gmail.com>
2019-02-17 09:28:17 +0200

    Constructor: capacity, pointers, ... init status table

--- heap.cpp
+++ heap.cpp
@@ -1,7 +1,14 @@
 #include "heap.h"
+#include <cstring>
 
 Heap::Heap(void* memory, size_t memorySize, size_t blockSize)
 {
+	this->blockSize = blockSize;
+	status = (char *)memory;
+	freeSpace = capacity = memorySize / (blockSize + 1);
+	if (capacity == 0) return;
+	buffer = status + capacity;
+	memset(status, 0, capacity);
 }
 
 void* Heap::allocate()
--- heap.h
+++ heap.h
@@ -3,6 +3,11 @@
 
 class Heap
 {
+	char* status;
+	char* buffer;
+	size_t blockSize;
+	size_t capacity;
+	size_t freeSpace;
 public:
 	Heap(void* memory, size_t memorySize, size_t blockSize);
 	void* allocate();
